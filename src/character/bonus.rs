cc66d4393f955eaf7458a36297e78b0d0b31fb6a
baptistemanson <baptiste.manson@inovia.fr>
2021-01-02 08:26:55 -0800

    trying functions

--- src/character/bonus.rs
+++ src/character/bonus.rs
@@ -5,6 +5,24 @@ pub struct ResolvedBonus {
     pub details: String,
 }
 
+#[allow(dead_code)]
+pub struct AvailableBonus {
+    pub name: BonusType,
+    pub roll_type: RollType,
+    pub resolve: dyn Fn(i32) -> i32,
+}
+#[allow(dead_code)]
+pub enum BonusType {
+    Deadly,
+    Striking,
+    Bless
+}
+#[allow(dead_code)]
+pub enum RollType {
+    AttackRoll,
+    DamageRollPreCrit,
+    DamageRollPostCrit,
+}
 
 // pub fn get_bonuses(character: &Character) -> Vec<Bonus> {
 //     character
