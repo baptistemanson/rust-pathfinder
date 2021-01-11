0d60b3093c9b7707e4463b13fd8b36cc45633a8a
Baptiste Manson <baptiste.manson@gmail.com>
2021-01-10 22:43:45 -0800

    clarify process, cleanup

--- app/src/tiles.rs
+++ app/src/tiles.rs
@@ -9,7 +9,6 @@ use crate::{
 pub struct TilesRenderer {
     vertex_buf: wgpu::Buffer,
     index_buf: wgpu::Buffer,
-    index_format: wgpu::IndexFormat,
     index_count: usize,
     bind_group: wgpu::BindGroup,
     pipeline: wgpu::RenderPipeline,
@@ -22,6 +21,16 @@ impl crate::Renderer for TilesRenderer {
         wgt::Features::NON_FILL_POLYGON_MODE
     }
 
+    // Describe each bind group layout
+    // Assemble bind group layouts into a pipeline layout (aka bind groups[])
+    // Describe the vertex layout
+    // Load the shaders
+    // Create the pipeline (aka the shaders + bind groups[] + vertex layout)
+    //
+    // Create the resources
+    // Assemble resources in a bind group
+    // Create vertex buffers
+    // And... done!
     fn init(
         sc_desc: &wgpu::SwapChainDescriptor,
         device: &wgpu::Device,
@@ -29,23 +38,7 @@ impl crate::Renderer for TilesRenderer {
     ) -> Self {
         use std::mem;
 
-        // Create the vertex and index buffers
-        let vertex_size = mem::size_of::<vertex::Vertex>();
-        let (vertex_data, index_data) = vertex::quad();
-
-        let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Vertex Buffer"),
-            contents: cast_slice(&vertex_data), // checks if a range of bytes can be turned into another and just do it. Works well to turn Struct into u8
-            usage: wgpu::BufferUsage::VERTEX,
-        });
-
-        let index_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Index Buffer"),
-            contents: cast_slice(&index_data),
-            usage: wgpu::BufferUsage::INDEX,
-        });
-
-        // Create pipeline layout, starting with the bind group layout
+        // Describe bind group layout
         let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             label: None,
             entries: &[
@@ -90,30 +83,83 @@ impl crate::Renderer for TilesRenderer {
                 },
             ],
         });
+        // Describe the pipeline layout, which is simply a collection of bind groups
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: None,
             bind_group_layouts: &[&bind_group_layout],
             push_constant_ranges: &[],
         });
 
-        // Create the texture
+        // Describe the vertex layout
+        let vertex_layout = wgpu::VertexStateDescriptor {
+            index_format: Some(wgpu::IndexFormat::Uint16),
+            vertex_buffers: &[wgpu::VertexBufferDescriptor {
+                stride: mem::size_of::<vertex::Vertex>() as wgpu::BufferAddress,
+                step_mode: wgpu::InputStepMode::Vertex,
+                attributes: &[
+                    wgpu::VertexAttributeDescriptor {
+                        format: wgpu::VertexFormat::Float4,
+                        offset: 0,
+                        shader_location: 0,
+                    },
+                    // wgpu::VertexAttributeDescriptor {
+                    //     format: wgpu::VertexFormat::Float2,
+                    //     offset: 4 * 4, // 4 float 32
+                    //     shader_location: 1,
+                    // },
+                ],
+            }],
+        };
 
+        // Load shaders
+        let vs_module =
+            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.vert.spv"));
+        let fs_module =
+            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
+
+        // Create the pipeline, with all the bind_groups layout + vertex layout + shaders
+        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                ..Default::default()
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: sc_desc.format,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
+            depth_stencil_state: None,
+            vertex_state: vertex_layout.clone(),
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        // Create resources
         let texture_tiles = texture(&device, &queue, image_tex("./assets/tiles.png"));
         let texture_mask = texture(&device, &queue, mask_bit_tex());
-
         let sampler = sampler(&device);
 
-        // Buffer
-        //convenient for everything but Samplers and Textures.
-        let dim_tiles = [8. as f32, 6. as f32];
         let uniform_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Uniform Buffer transform"),
-            contents: cast_slice(&dim_tiles), // [f32] => [u8]
+            label: Some("Dimension of tiles"),
+            contents: cast_slice(&[8. as f32, 6. as f32]), // [f32] => [u8]
             usage: wgpu::BufferUsage::UNIFORM | wgpu::BufferUsage::COPY_DST,
         });
-        // Bind groups!
 
-        // Create bind group
+        // Create a bind group, which is a collection of resources
         let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
             layout: &bind_group_layout,
             entries: &[
@@ -140,79 +186,31 @@ impl crate::Renderer for TilesRenderer {
             ],
             label: None,
         });
+        // Create the vertex and index buffers
+        let (vertex_data, index_data) = vertex::quad();
 
-        let index_format = wgpu::IndexFormat::Uint16;
-
-        // Vertices description
-        let vertex_state = wgpu::VertexStateDescriptor {
-            index_format: Some(index_format),
-            vertex_buffers: &[wgpu::VertexBufferDescriptor {
-                stride: vertex_size as wgpu::BufferAddress,
-                step_mode: wgpu::InputStepMode::Vertex,
-                attributes: &[
-                    wgpu::VertexAttributeDescriptor {
-                        format: wgpu::VertexFormat::Float4,
-                        offset: 0,
-                        shader_location: 0,
-                    },
-                    wgpu::VertexAttributeDescriptor {
-                        format: wgpu::VertexFormat::Float2,
-                        offset: 4 * 4, // 4 float 32
-                        shader_location: 1,
-                    },
-                ],
-            }],
-        };
-
-        let vs_module =
-            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.vert.spv"));
-        let fs_module =
-            device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
-
-        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: None,
-            layout: Some(&pipeline_layout),
-            vertex_stage: wgpu::ProgrammableStageDescriptor {
-                module: &vs_module,
-                entry_point: "main",
-            },
-            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
-                module: &fs_module,
-                entry_point: "main",
-            }),
-            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: wgpu::CullMode::Back,
-                ..Default::default()
-            }),
-            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
-            color_states: &[wgpu::ColorStateDescriptor {
-                format: sc_desc.format,
-                color_blend: wgpu::BlendDescriptor::REPLACE,
-                alpha_blend: wgpu::BlendDescriptor::REPLACE,
-                write_mask: wgpu::ColorWrite::ALL,
-            }],
-            depth_stencil_state: None,
-            vertex_state: vertex_state.clone(),
-            sample_count: 1,
-            sample_mask: !0,
-            alpha_to_coverage_enabled: false,
+        let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: cast_slice(&vertex_data), // checks if a range of bytes can be turned into another and just do it. Works well to turn Struct into u8
+            usage: wgpu::BufferUsage::VERTEX,
         });
 
-        // Done
+        let index_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: cast_slice(&index_data),
+            usage: wgpu::BufferUsage::INDEX,
+        });
+
         TilesRenderer {
+            pipeline,
             vertex_buf,
             index_buf,
-            index_format,
             index_count: index_data.len(),
             bind_group,
-            pipeline,
         }
     }
 
-    fn update(&mut self, _event: winit::event::WindowEvent) {
-        //empty
-    }
+    fn update(&mut self, _event: winit::event::WindowEvent) {}
 
     fn resize(
         &mut self,
@@ -222,6 +220,13 @@ impl crate::Renderer for TilesRenderer {
     ) {
     }
 
+    // Create command encoder
+    // Create render pass
+    // => Pick pipeline
+    // => Pick bind group
+    // => Pick index and vertex buffers
+    // => Put Draw instruction in the render pass
+    // Submit render pass to queue
     fn render(
         &mut self,
         frame: &wgpu::SwapChainTexture,
@@ -252,7 +257,7 @@ impl crate::Renderer for TilesRenderer {
             rpass.push_debug_group("Prepare data for draw.");
             rpass.set_pipeline(&self.pipeline);
             rpass.set_bind_group(0, &self.bind_group, &[]);
-            rpass.set_index_buffer(self.index_buf.slice(..), self.index_format);
+            rpass.set_index_buffer(self.index_buf.slice(..), wgpu::IndexFormat::Uint16);
             rpass.set_vertex_buffer(0, self.vertex_buf.slice(..));
             rpass.pop_debug_group();
             rpass.insert_debug_marker("Draw!");
